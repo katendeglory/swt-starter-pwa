f72803db4b553f9b2e1e7c1cf1288ee43db25aab
Glory Katende <katendeglory@gmail.com>
2021-03-29 01:24:23 +0200

    Yet another commit

--- src-sw.js
+++ src-sw.js
@@ -1,23 +1,28 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
 workbox.routing.registerRoute(
   new RegExp('https://jsonplaceholder.typicode.com/users'),
   workbox.strategies.cacheFirst()
 );
 
-workbox.routing.registerRoute(
-  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.cacheFirst({
-    cacheName: 'google-fonts',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-      }),
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      }),
-    ],
-  }),
-);
+// workbox.routing.registerRoute(
+//   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+//   workbox.strategies.cacheFirst({
+//     cacheName: 'google-fonts',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 30,
+//       }),
+//       new workbox.cacheableResponse.Plugin({
+//         statuses: [0, 200]
+//       }),
+//     ],
+//   }),
+// );
 
 workbox.precaching.precacheAndRoute([]);
